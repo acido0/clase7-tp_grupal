2e26433cbccb65d2ad263c4d2e3a23d58cc58197
acido0 <valenvazquez606@gmail.com>
2022-04-26 20:37:49 -0300

    Salto
    Agregamos la condición de salto y constantes

--- Scenes/Personaje/Personaje.gd
+++ Scenes/Personaje/Personaje.gd
@@ -1,27 +1,30 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
-var gravity = 25
+const gravity = 25
+const jump = -500
+const xmove_limit = 250
+const normal_floor = Vector2.UP
 
 func _physics_process(delta):
 	velocity.y += gravity
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 40
+	if Input.is_action_pressed("ui_right") and velocity.x < xmove_limit: 
+		velocity.x += 75
+		
 	
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 40
+	if Input.is_action_pressed("ui_left") and velocity.x > -xmove_limit:
+		velocity.x -= 75
 	
 	if Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left"):
 		velocity.x = 0
 	
-	
-	
-	
-	
-	
-	
-	
-	move_and_slide(velocity)
+	if is_on_floor(): 
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = jump
+		
+		
+		
+	move_and_slide(velocity, normal_floor)
 	
 	
 	
