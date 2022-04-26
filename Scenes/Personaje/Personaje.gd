e2381f91ec17452764fde47a56b5db673f32b919
Leonardo Maidana <leo-lomas1@hotmail.com>
2022-04-26 20:55:19 -0300

    Doble_Salto
    Se agrego contador de saltos y la implemetacion del doble salto

--- Scenes/Personaje/Personaje.gd
+++ Scenes/Personaje/Personaje.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
+var contador_saltos = 0
 const gravity = 25
 const jump = -500
 const xmove_limit = 250
@@ -18,12 +19,17 @@ func _physics_process(delta):
 	if Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left"):
 		velocity.x = 0
 	
-	if is_on_floor(): 
+	if is_on_floor():
+		contador_saltos = 0
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = jump
-		
-		
-		
+			contador_saltos += 1
+			
+	else:
+		if Input.is_action_just_pressed("ui_up") and contador_saltos == 1:
+			velocity.y = jump
+			contador_saltos += 1
+			
 	move_and_slide(velocity, normal_floor)
 	
 	
