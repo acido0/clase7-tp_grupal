873922bff22c637a4a9e69b20eac0848a2825d28
acido0 <valenvazquez606@gmail.com>
2022-04-26 20:04:21 -0300

    Movimientos1

--- Scenes/Personaje/Personaje.gd
+++ Scenes/Personaje/Personaje.gd
@@ -1,1 +1,29 @@
 extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 40
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 40
+	
+	if Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left"):
+		velocity.x = 0
+	
+	
+	
+	
+	
+	
+	
+	
+	move_and_slide(velocity)
+	
+	
+	
+	
+	pass
+	
