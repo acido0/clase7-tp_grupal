76f49442f585bbf07c7bcabac7b09983a692e9c3
KarismaCepita <101679115+KarismaCepita@users.noreply.github.com>
2022-04-26 21:53:06 -0300

    Add files via upload

--- Scenes/Personaje/Personaje.gd
+++ Scenes/Personaje/Personaje.gd
@@ -11,25 +11,29 @@ func _physics_process(delta):
 	velocity.y += gravity
 	if Input.is_action_pressed("ui_right") and velocity.x < xmove_limit: 
 		velocity.x += 75
-		
-	
+		if contador_saltos == 0:
+			$AnimatedSprite.play("caminar")
+			$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("ui_left") and velocity.x > -xmove_limit:
 		velocity.x -= 75
-	
+		if contador_saltos == 0:
+			$AnimatedSprite.play("caminar")
+			$AnimatedSprite.flip_h = true
 	if Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left"):
 		velocity.x = 0
-	
+	if contador_saltos == 0 and velocity.x == 0:
+		$AnimatedSprite.play("quieto")
 	if is_on_floor():
 		contador_saltos = 0
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = jump
 			contador_saltos += 1
-			
+			$AnimatedSprite.play("salto")
 	else:
 		if Input.is_action_just_pressed("ui_up") and contador_saltos == 1:
 			velocity.y = jump
 			contador_saltos += 1
-			
+			$AnimatedSprite.play("doble salto")
 	move_and_slide(velocity, normal_floor)
 	
 	
